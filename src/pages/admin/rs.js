2535c4687bc71505e8489571f6ebe50ba3269272
Jezh Technologies <148617646+jezhtech@users.noreply.github.com>
2023-12-19 17:48:50 +0530

    Update rs.js

--- src/pages/admin/rs.js
+++ src/pages/admin/rs.js
@@ -14,7 +14,7 @@ const[schools]=useCollectionData(db.collection("schools"))
     <Head>
  <meta charset="utf-8"/>
  <meta name="viewport" content="width=device-width, initial-scale=1"/>
- <title>Latest News</title>
+ <title>Registerd schools</title>
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossOrigin="anonymous"/>
 
  </Head>
